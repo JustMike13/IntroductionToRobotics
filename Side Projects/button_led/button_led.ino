2a735a7f242003e5b3c561366709e4a5fcd6d0ae
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2021-10-26 20:13:30 +0300

    add first side project

---
+++ Side Projects/button_led/button_led.ino
@@ -0,0 +1,24 @@
+// LED is on only while the button is pressed
+
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
