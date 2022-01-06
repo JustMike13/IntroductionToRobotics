07aff3e30059b7fe8e2a215b203cd93db21a3df4
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2022-01-06 15:56:19 +0200

    commit

---
+++ Lab/servoMotor/servoMotor.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+int servoPin = 9;
+Servo myServo;
+int angle = 0;
+
+void setup() {
+  myServo.attach(servoPin);
+
+}
+
+void loop() {
+  for (angle = 0; angle < 180; angle ++){
+    myServo.write(angle);
+    delay(15);
+  }
+  for (angle = 180; angle > 0; angle --){
+    myServo.write(angle);
+    delay(15);
+  }
+
+}
